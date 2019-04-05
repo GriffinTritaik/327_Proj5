9f5bba7084dc8af5ddab2dba98c43f9fbd624d0b
guest-vs6d6q <guest-vs6d6q@L221-0000>
2019-04-05 12:44:44 -0400

    started

--- Debug/objects.mk
+++ Debug/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS :=
+LIBS := -l327_Proj5_Lib
 
---
+++ Debug/src/327_Proj5.d
@@ -0,0 +1,25 @@
+src/327_Proj5.o: ../src/327_Proj5.cpp \
+ ../src/../../327_Proj5_Lib/includes/Smalltalk_American.h \
+ ../src/../../327_Proj5_Lib/includes/Smalltalk.h \
+ ../src/../../327_Proj5_Lib/includes/Watch.h \
+ ../src/../../327_Proj5_Lib/includes/ST_American_DonutEnthusiest.h \
+ ../src/../../327_Proj5_Lib/includes/Smalltalk_Brit.h \
+ ../src/../../327_Proj5_Lib/includes/Watch.h \
+ ../src/../../327_Proj5_Lib/includes/constants.h \
+ ../src/../../327_Proj5_Lib/includes/Functions.h
+
+../src/../../327_Proj5_Lib/includes/Smalltalk_American.h:
+
+../src/../../327_Proj5_Lib/includes/Smalltalk.h:
+
+../src/../../327_Proj5_Lib/includes/Watch.h:
+
+../src/../../327_Proj5_Lib/includes/ST_American_DonutEnthusiest.h:
+
+../src/../../327_Proj5_Lib/includes/Smalltalk_Brit.h:
+
+../src/../../327_Proj5_Lib/includes/Watch.h:
+
+../src/../../327_Proj5_Lib/includes/constants.h:
+
+../src/../../327_Proj5_Lib/includes/Functions.h:
